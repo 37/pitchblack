991bea44f41c0f9371a8151f12187846c96bcb0c
Karthik Rajgopal <karthik.cool7777@gmail.com>
2015-11-03 07:29:02 +1100

    lots of changes

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
 
 var app = express();
 
@@ -20,7 +19,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/', routes);
+app.use('/', require('./routes/index'));
+app.use('/ani', require('./routes/ani'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
